6bfbe3902d55a8cfb65f9f195628b45f327d9339
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-10-30 15:13:16 -0300

    Created Navbar components

---
+++ components/NavbarActions.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Button from "@/components/ui/Button";
+// import useCart from "@/hooks/use-cart";
+
+const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => { setIsMounted(true) }, []);
+
+  const router = useRouter();
+  // const cart = useCart();
+
+  if (!isMounted) return null;
+
+  return ( 
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button className="flex items-center rounded-full bg-black px-4 py-2" onClick={() => router.push('/cart')}>
+        <ShoppingBag color="white" size={20} />
+        {/* {cart.items.length} */}
+        <span className="ml-2 text-sm font-medium text-white">0</span>
+      </Button>
+    </div>
+  );
+};
+export default NavbarActions;
